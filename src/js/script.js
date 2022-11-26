ef65c219d454d1ff3f3b35a5e387602ac494c4b5
Doniyor <donishodiev@gmail.com>
2022-11-26 08:03:56 -0500

    Fitness

--- src/js/script.js
+++ src/js/script.js
@@ -201,3 +201,13 @@ function showModalByName(){
 
 showModalByName();
 
+const pageUp = document.querySelector(".pageup");
+
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY >= 1500){
+    pageUp.style.display = 'block'
+  } else{
+    pageUp.style.display = 'none';
+  }
+});
